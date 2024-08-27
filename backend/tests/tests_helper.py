fb58941dc452310b7323b1755b1c0c96fe2ff826
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2024-08-28 00:55:35 +0200

    [backend] - test: Add sample test

---
+++ backend/tests/tests_helper.py
@@ -0,0 +1,8 @@
+from rest_framework.test import APITestCase
+
+
+class TestSetUp(APITestCase):
+    """
+        Test case class that configures common resources and configurations for API testing.
+    """
+    pass
