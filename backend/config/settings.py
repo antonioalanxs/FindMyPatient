460cc3107b0bb7b369ebe4132e2e8b3ed43dc7a7
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2024-09-12 11:29:19 +0200

    [backend] - feat: Configure Mailtrap

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,6 +104,14 @@ LOCAL_MIDDLEWARE = []
 
 MIDDLEWARE = BASE_MIDDLEWARE + THIRD_PARTY_MIDDLEWARE + LOCAL_MIDDLEWARE
 
+# Mail sending configuration w/ Mailtrap
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
