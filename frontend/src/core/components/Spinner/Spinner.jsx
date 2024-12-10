9e0f6d7966c3ceffab6b9dd287d4c03141810874
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2024-12-10 18:13:37 +0100

    [frontend] - feat: Make spinner component customizable

--- frontend/src/core/components/Spinner/Spinner.jsx
+++ frontend/src/core/components/Spinner/Spinner.jsx
@@ -1,11 +1,13 @@
-/**
- * It displays a loading spinner.
- *
- * @returns {JSX.Element} - The component.
- */
-function Spinner() {
+function Spinner({ large, primary }) {
   return (
-    <div className="spinner-border" role="status">
+    <div
+      className={`spinner-border ${primary && "text-primary"}`}
+      style={{
+        width: large && "3rem",
+        height: large && "3rem",
+      }}
+      role="status"
+    >
       <span className="visually-hidden">Loading...</span>
     </div>
   );
