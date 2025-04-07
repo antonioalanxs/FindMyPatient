458ac157debac9d135ad6ae5729cb51f99e6d42b
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-07 18:37:21 +0200

    [backend] - feat: Add get a group

--- backend/groups/views.py
+++ backend/groups/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group
+from django.shortcuts import get_object_or_404
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -22,6 +23,12 @@ class GroupViewSet(
     queryset = None
     serializer_class = GroupSerializer
 
+    def get_object(self):
+        return get_object_or_404(
+            self.model,
+            id=self.kwargs.get("pk")
+        )
+
     @method_permission_classes([IsAuthenticated, IsAdministrator])
     def list(self, request, *args, **kwargs):
         queryset = self.search(self.model, request)
@@ -30,3 +37,9 @@ class GroupViewSet(
             queryset,
             self.serializer_class
         )
+
+    @method_permission_classes([IsAuthenticated, IsAdministrator])
+    def retrieve(self, request, *args, **kwargs):
+        group = self.get_object()
+        serializer = self.serializer_class(group)
+        return Response(serializer.data, status=status.HTTP_200_OK)
