56d7ce5506627a4c4c0702c241bd414f677a0098
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-08 18:56:15 +0200

    [frontend] - feat: Add create administrator

--- backend/administrators/views.py
+++ backend/administrators/views.py
@@ -1,13 +1,18 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Administrator
-from .serializers import AdministratorPreviewSerializer
+from .serializers import (
+    AdministratorPreviewSerializer,
+    AdministratorSerializer
+)
 from permissions.decorators import method_permission_classes
 from permissions.users import IsAdministrator
 from mixins.search import SearchMixin
 from mixins.pagination import PaginationMixin
 from mixins.serializers import SerializerValidationErrorResponseMixin
+from mixins.emails import EmailMixin
 
 
 class AdministratorViewSet(
@@ -16,10 +21,13 @@ class AdministratorViewSet(
     SearchMixin,
     PaginationMixin,
     SerializerValidationErrorResponseMixin,
+    EmailMixin
 ):
     lookup_field = 'id'
     model = Administrator
     queryset = None
+    list_serializer_class = AdministratorPreviewSerializer
+    serializer_class = AdministratorSerializer
 
     @method_permission_classes([IsAuthenticated, IsAdministrator])
     def list(self, request, *args, **kwargs):
@@ -31,5 +39,31 @@ class AdministratorViewSet(
         return self.get_paginated_response_(
             request,
             queryset,
-            AdministratorPreviewSerializer
+            self.list_serializer_class
         )
+
+    @method_permission_classes([IsAuthenticated, IsAdministrator])
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            administrator, random_password = serializer.save()
+
+            self.send_email(
+                'emails/sign_up_email_template.html',
+                administrator.email,
+                'Sign up',
+                {
+                    'first_name': administrator.first_name,
+                    'last_name': administrator.last_name,
+                    'username': administrator.identity_card_number,
+                    'password': random_password
+                }
+            )
+
+            return Response(
+                {'message': 'Administrator created successfully.'},
+                status=status.HTTP_201_CREATED
+            )
+
+        return self.handle_serializer_is_not_valid_response(serializer)
--- backend/tests/test_administrators.py
+++ backend/tests/test_administrators.py
@@ -27,3 +27,51 @@ class AdministratorListTestCase(TestSetUp):
     def test_list_administrators_with_non_authenticated_user(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AdministratorCreateTestCase(TestSetUp):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("administrators-list")
+
+        self.input = {
+            "first_name": "Antonio José",
+            "last_name": "Alanís Bernal",
+            "identity_card_number": "54182062L",
+            "social_security_code": "281628165548",
+            "email": "antoniojalanis7131@gmail.com",
+            "phone_number": "+34684222019",
+            "birth_date": "2002-07-04",
+            "gender": "M",
+            "nationality": "ES"
+        }
+
+    def test_create_administrator(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_existing_administrator(self):
+        self.client.force_authenticate(user=self.administrator)
+        self.client.post(self.url, data=self.input)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_administrator_with_malformed_input(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.post(self.url, data={})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_administrator_with_doctor(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_administrator_with_patient(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_administrator_with_non_authenticated_user(self):
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
