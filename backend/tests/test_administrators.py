65ff1f182a1a2cfdb9a9f3ac1e272a8941cb5961
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-08 17:58:46 +0200

    [backend] - test: Add tests for list administrators

---
+++ backend/tests/test_administrators.py
@@ -0,0 +1,29 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+
+from .tests_helper import TestSetUp
+
+
+class AdministratorListTestCase(TestSetUp):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("administrators-list")
+
+    def test_list_administrators(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_administrators_with_doctor(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_administrators_with_patient(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_administrators_with_non_authenticated_user(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
