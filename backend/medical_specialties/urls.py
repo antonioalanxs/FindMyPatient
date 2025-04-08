7a9830036503c20355ae464902b6343809f3dcdb
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-08 10:53:44 +0200

    [backend] - feat: Add get medical specialty and list doctors by medical specialty

--- backend/medical_specialties/urls.py
+++ backend/medical_specialties/urls.py
@@ -1,8 +1,16 @@
+from django.urls import path
+
 from rest_framework.routers import DefaultRouter
 
-from .views import MedicalSpecialtyViewSet
+from .views import MedicalSpecialtyViewSet, ListDoctorsByMedicalSpecialtyAPIView
 
 router = DefaultRouter()
 router.register(r'', MedicalSpecialtyViewSet, basename='medical_specialties')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path(
+        '<int:pk>/doctors',
+        ListDoctorsByMedicalSpecialtyAPIView.as_view(),
+        name='medical_specialties-doctors'
+    ),
+]
--- backend/medical_specialties/views.py
+++ backend/medical_specialties/views.py
@@ -2,15 +2,43 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status, viewsets, mixins
+from rest_framework import status, viewsets, mixins, views
 
 from .serializers import MedicalSpecialtySerializer
+from doctors.serializers import DoctorPreviewSerializer
 from .models import MedicalSpecialty
+from doctors.models import Doctor
 from mixins.search import SearchMixin
 from mixins.pagination import PaginationMixin
 from mixins.serializers import SerializerValidationErrorResponseMixin
 from permissions.decorators import method_permission_classes
 from permissions.users import IsAdministrator
+
+
+class ListDoctorsByMedicalSpecialtyAPIView(
+    views.APIView,
+    SearchMixin,
+    PaginationMixin
+):
+    permission_classes = [IsAuthenticated, IsAdministrator]
+
+    def get(self, request, *args, **kwargs):
+        medical_specialty = get_object_or_404(
+            MedicalSpecialty,
+            id=self.kwargs.get("pk")
+        )
+
+        queryset = self.search(
+            Doctor,
+            request,
+            base_queryset=medical_specialty.doctor_set.all()
+        )
+
+        return self.get_paginated_response_(
+            request,
+            queryset,
+            DoctorPreviewSerializer
+        )
 
 
 class MedicalSpecialtyViewSet(
@@ -24,6 +52,12 @@ class MedicalSpecialtyViewSet(
     queryset = None
     serializer_class = MedicalSpecialtySerializer
 
+    def get_object(self):
+        return get_object_or_404(
+            self.model,
+            id=self.kwargs.get("pk")
+        )
+
     @method_permission_classes([IsAuthenticated, IsAdministrator])
     def list(self, request, *args, **kwargs):
         queryset = self.search(self.model, request)
@@ -32,3 +66,9 @@ class MedicalSpecialtyViewSet(
             queryset,
             self.serializer_class
         )
+
+    @method_permission_classes([IsAuthenticated, IsAdministrator])
+    def retrieve(self, request, *args, **kwargs):
+        medical_specialty = self.get_object()
+        serializer = self.serializer_class(medical_specialty)
+        return Response(serializer.data, status=status.HTTP_200_OK)
