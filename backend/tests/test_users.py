d1f153786e45317c9fa63e7fd4f6f4ad9ab5fca4
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-09 01:42:22 +0200

    [backend] - tests: Add tests for delete user

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -92,3 +92,43 @@ class UserUpdateTestCase(TestSetUp):
         self.client.force_authenticate(user=self.user)
         response = self.client.patch(self.url(self.user.id), data={"birth_date": ""})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UserDestroyTestCase(TestSetUp):
+    def setUp(self):
+        super().setUp()
+        self.url = lambda id: reverse("users-detail", kwargs={"id": id})
+
+    def test_destroy_patient_with_administrator(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_destroy_user_with_non_authenticated_user(self):
+        response = self.client.delete(self.url(self.user.id))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_self_destroy_yourself_as_administrator(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.delete(self.url(self.administrator.id))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_destroy_patient(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_destroy_non_existing_user(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.delete(self.url(self.non_existing_id))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_destroy_patient_who_is_not_assigned_to_a_doctor(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.delete(self.url(self.patient_without_address_and_primary_doctor.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_destroy_patient_with_patient(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
