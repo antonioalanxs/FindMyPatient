fa97a33e7f15519a0e43bae20ffd30164afb543f
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-09 01:42:11 +0200

    [backend] - refactor: Quit delete patient

--- backend/tests/test_patients.py
+++ backend/tests/test_patients.py
@@ -74,46 +74,6 @@ class PatientUpdateTestCase(TestSetUp):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class PatientDestroyTestCase(TestSetUp):
-    def setUp(self):
-        super().setUp()
-        self.url = lambda id: reverse("patients-detail", kwargs={"id": id})
-
-    def test_destroy_patient(self):
-        self.client.force_authenticate(user=self.doctor)
-        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    def test_destroy_non_existing_patient(self):
-        self.client.force_authenticate(user=self.doctor)
-        response = self.client.delete(self.url(self.non_existing_id))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_destroy_patient_who_is_not_assigned_to_a_doctor(self):
-        self.client.force_authenticate(user=self.doctor)
-        response = self.client.delete(self.url(self.patient_without_address_and_primary_doctor.id))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_destroy_patient_with_patient(self):
-        self.client.force_authenticate(user=self.user)
-        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_destroy_patient_with_administrator(self):
-        self.client.force_authenticate(user=self.administrator)
-        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    def test_destroy_patient_with_owner_user(self):
-        self.client.force_authenticate(user=self.patient_with_address_and_primary_doctor)
-        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_destroy_patient_with_non_authenticated_user(self):
-        response = self.client.delete(self.url(self.patient_with_address_and_primary_doctor.id))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-
 class PatientCreateTestCase(TestSetUp):
     def setUp(self):
         super().setUp()
