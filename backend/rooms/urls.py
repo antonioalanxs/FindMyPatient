d0e8f1f19b4f580fe228fd770e40c5c23b478bb3
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-23 01:10:19 +0200

    [backend] - feat: Add list rooms

--- backend/rooms/urls.py
+++ backend/rooms/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+
+from .views import RoomViewSet
+
+router = DefaultRouter()
+router.register(r'', RoomViewSet, basename='rooms')
+
+urlpatterns = router.urls
--- backend/rooms/views.py
+++ backend/rooms/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import (
+    viewsets,
+    mixins
+)
+
+from .serializers import (
+    RoomPreviewSerializer,
+)
+from mixins.search import SearchMixin
+from mixins.pagination import PaginationMixin
+from mixins.serializers import SerializerValidationErrorResponseMixin
+from permissions.decorators import method_permission_classes
+from permissions.users import IsAdministrator
+from rooms.models import Room
+
+
+class RoomViewSet(
+    viewsets.GenericViewSet,
+    mixins.DestroyModelMixin,
+    SearchMixin,
+    PaginationMixin,
+    SerializerValidationErrorResponseMixin
+):
+    model = Room
+    queryset = None
+    list_serializer_class = RoomPreviewSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.model,
+            id=self.kwargs.get("pk")
+        )
+
+    @method_permission_classes([IsAuthenticated, IsAdministrator])
+    def list(self, request, *args, **kwargs):
+        queryset = self.search(self.model, request)
+        return self.get_paginated_response_(
+            request,
+            queryset,
+            self.list_serializer_class
+        )
