3d25e8aa41fe2c01ea38e1de2bc7ec6af545fa88
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-23 00:27:42 +0200

    [backend] - feat/fix: Add Room model and Translate medical specialties to English

--- backend/rooms/models.py
+++ backend/rooms/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from medical_specialties.models import MedicalSpecialty
+
+
+class Room(models.Model):
+    class Meta:
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+
+    description = models.TextField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+
+    location = models.CharField(
+        max_length=50,
+    )
+
+    capacity = models.IntegerField(
+        blank=True,
+        null=True,
+    )
+
+    medical_specialty = models.ForeignKey(
+        MedicalSpecialty,
+        on_delete=models.CASCADE,
+        related_name="rooms",
+        null=True,
+    )
+
+    is_available = models.BooleanField(
+        default=True,
+    )
