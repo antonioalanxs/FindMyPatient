b0351fa1ad7e4f73a0e00cec5262161b26170ec1
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-23 00:51:41 +0200

    [backend] - refactor: Fix "medical_specialty" field of RoomSerializer

--- backend/rooms/serializers.py
+++ backend/rooms/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from medical_specialties.serializers import MedicalSpecialtySqueezeSerializer
 from .models import Room
 
 
 class RoomPreviewSerializer(serializers.ModelSerializer):
+    medical_specialty = MedicalSpecialtySqueezeSerializer(read_only=True)
+
     class Meta:
         model = Room
         fields = [
