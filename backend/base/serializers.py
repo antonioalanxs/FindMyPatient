489f51e6af3cbb865366383d254fddb261e04068
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-22 17:32:10 +0200

    [backend] - fix: Add "id" field to UserSerializer

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -14,6 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            "id",
             "identity_card_number",
             "first_name",
             "last_name",
