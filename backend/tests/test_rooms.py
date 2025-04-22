6daa078a6d0fb0946506596e077be36c87512b26
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-23 01:22:59 +0200

    [backend] - tests: Add tests for get room

--- backend/tests/test_rooms.py
+++ backend/tests/test_rooms.py
@@ -56,3 +56,32 @@ class RoomDestroyTestCase(TestSetUp):
     def test_destroy_room_with_non_authenticated_user(self):
         response = self.client.delete(self.url(self.room.id))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+class RoomRetrieveTestCase(TestSetUp):
+    def setUp(self):
+        super().setUp()
+        self.url = lambda id: reverse("rooms-detail", kwargs={"pk": id})
+
+    def test_retrieve_room(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.get(self.url(self.room.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_non_existing_room(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.get(self.url(self.non_existing_id))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_retrieve_room_with_patient(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(self.url(self.room.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_retrieve_room_with_doctor(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.get(self.url(self.room.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_retrieve_room_with_non_authenticated_user(self):
+        response = self.client.get(self.url(self.room.id))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
