484a1dd17f8cbec8607e9876045e6204c455a986
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-22 18:18:41 +0200

    [frontend] - tests: Fix tests

--- frontend/src/modules/in/components/DoctorInformation/DoctorInformation.test.jsx
+++
@@ -1,8 +0,0 @@
-import { customRender } from "@/tests/customRender";
-import DoctorInformation from "@/modules/in/components/DoctorInformation/DoctorInformation";
-
-describe("DoctorInformation", () => {
-  it("should create", () => {
-    customRender(<DoctorInformation doctor={{}} />);
-  });
-});
--- frontend/src/modules/in/components/PatientInformation/PatientInformation.test.jsx
+++
@@ -1,8 +0,0 @@
-import { customRender } from "@/tests/customRender";
-import PatientInformation from "@/modules/in/components/PatientInformation/PatientInformation";
-
-describe("PatientInformation", () => {
-  it("should create", () => {
-    customRender(<PatientInformation patient={{}} />);
-  });
-});
