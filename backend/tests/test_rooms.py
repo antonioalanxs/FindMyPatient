9088678d5fb10c3d3efc6d1c183a5caf72df4111
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-04-23 02:39:20 +0200

    [backend] - tests: Add tests for create room

--- backend/tests/test_rooms.py
+++ backend/tests/test_rooms.py
@@ -28,6 +28,7 @@ class RoomListTestCase(TestSetUp):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
 class RoomDestroyTestCase(TestSetUp):
     def setUp(self):
         super().setUp()
@@ -56,7 +57,8 @@ class RoomDestroyTestCase(TestSetUp):
     def test_destroy_room_with_non_authenticated_user(self):
         response = self.client.delete(self.url(self.room.id))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        
+
+
 class RoomRetrieveTestCase(TestSetUp):
     def setUp(self):
         super().setUp()
@@ -85,3 +87,54 @@ class RoomRetrieveTestCase(TestSetUp):
     def test_retrieve_room_with_non_authenticated_user(self):
         response = self.client.get(self.url(self.room.id))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class RoomCreateTestCase(TestSetUp):
+    def setUp(self):
+        super().setUp()
+
+        self.url = reverse("rooms-list")
+
+        self.input = {
+            "name": "Room 0404",
+            "description": "Gynecological examination room with privacy curtains.",
+            "location": "4th Floor, South Wing",
+            "capacity": 2,
+            "medical_specialty": self.medical_specialty_id,
+            "availability": True
+        }
+
+    def test_create_room(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.post(self.url, data=self.input, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_existing_room(self):
+        self.client.force_authenticate(user=self.administrator)
+
+        self.client.post(self.url, data=self.input, format="json")
+
+        response = self.client.post(self.url, data=self.input, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_room_with_patient(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, data=self.input, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_room_with_doctor(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.post(self.url, data=self.input, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_room_with_non_authenticated_user(self):
+        response = self.client.post(self.url, data=self.input, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_room_with_malformed_input(self):
+        self.client.force_authenticate(user=self.administrator)
+
+        self.input["capacity"] = "test"
+
+        response = self.client.post(self.url, data=self.input, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
