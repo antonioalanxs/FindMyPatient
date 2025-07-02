7dcaf2c2abe02ba99a87c2c466e160a6e6c2138e
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2025-07-02 21:59:40 +0200

    [backend] - test: Add tests for create treatment

--- backend/tests/test_medical_tests.py
+++ backend/tests/test_medical_tests.py
@@ -54,3 +54,47 @@ class MedicalTestListTestCase(TestSetUp):
         self.client.force_authenticate(user=self.doctor)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class CreateMedicalTestCase(TestSetUp):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse('medical_tests-list')
+        self.input = {
+            "name": "Test Medical test",
+            "description": "Test description",
+            "date": "2023-10-01",
+            "patient": self.patient_with_address_and_primary_doctor.id,
+            "doctor": self.doctor.id,
+            "appointment": self.appointment.id
+        }
+
+    def test_create_medical_test_with_non_authenticated_user(self):
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_medical_test_with_patient(self):
+        self.client.force_authenticate(user=self.patient_with_address_and_primary_doctor)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_medical_test_with_doctor_with_an_assigned_patient(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_medical_test_with_doctor_without_an_assigned_patient(self):
+        self.client.force_authenticate(user=self.another_doctor)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_medical_test_with_administrator(self):
+        self.client.force_authenticate(user=self.administrator)
+        response = self.client.post(self.url, data=self.input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_medical_test_with_malformed_input(self):
+        self.client.force_authenticate(user=self.doctor)
+        malformed_input = self.input.copy()
+        malformed_input['date'] = 'invalid-date'
+        response = self.client.post(self.url, data=malformed_input)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
